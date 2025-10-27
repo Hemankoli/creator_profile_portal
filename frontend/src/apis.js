7f2ce8e5b3c9979f8b3fef9e682fa26363fce456
HemanKoli <hemankoli1409@gmail.com>
2025-10-27 14:17:39 +0530

    initial commits

--- frontend/src/apis.js
+++ frontend/src/apis.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-const BASE = import.meta.env.VITE_API_BASE || 'http://localhost:4000';
+const BASE = import.meta.env.VITE_API_BASE;
 export const api = axios.create({ baseURL: BASE });
-
 
 export const fetchCreators = (opts) => api.get('/creators', { params: opts }).then(r => r.data);
 export const fetchCreator = (id) => api.get(`/creator/${id}`).then(r => r.data);
